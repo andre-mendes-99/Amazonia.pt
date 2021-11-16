fa54443c95b6a3054c618dfd9ab0a86f91a94b6b
André de Oliveira Gomes Zépico Mendes <andrezmendes18@gmail.com>
2021-11-16 22:04:12 +0000

    Inicio do CodeFirst

---
+++ Amazonia.DAL/Modelo/AmazoniaContexto.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Amazonia.DAL.Modelo
+{
+    public class AmazoniaContexto : DbContext
+    {
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Morada> Moradas { get; set; }
+    }
+}
---
+++ Amazonia.DAL/Modelo/Entidade.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Amazonia.DAL.Modelo
+{
+    public abstract class Entidade
+    {
+        //[Key] // Desnecessário já que estamos optar por usar o mecanismo de convenção
+        //[Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        [Required]
+        //[MinLength(10)]
+        [MaxLength(255)]
+        public string Nome { get; set; }
+    }
+}
